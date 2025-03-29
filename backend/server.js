321643844285a9d33bf8b66498251f66363d96ea
Tareq Radi <raditareq15@gmail.com>
2025-03-29 15:27:25 +0200

    edit the limit

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //middleware
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173'],
     credentials: true,
 }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 //routes
 app.use('/api/auth', authRoute);
