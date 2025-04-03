8c3d51ef78ddd95ff257708087222b735491787a
Tareq Radi <raditareq15@gmail.com>
2025-04-03 14:14:46 +0200

    change to mongodb url drive

--- backend/config/connectDB.config.js
+++ backend/config/connectDB.config.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDb = async() => {
     try {
-        const connect = await mongoose.connect('mongodb://localhost:27017/e-commerce');
-        console.log(`Database is connected to ${connect.connection.host} ✔`);
+        // const connect = await mongoose.connect('mongodb://localhost:27017/e-commerce'); //for local
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+		console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.log('Database connection failed', error.message);
         process.exit(1);
